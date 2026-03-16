7d8ee572381b094196ca77037f13ea7bb422f147
Ravi Brennon Achay <rcachay@cvisc.pshs.edu.ph>
2026-03-16 13:26:41 +0800

    Excercise2

---
+++ CS1_Qtr2_Emerald_Excercise2CODE_Achay.py
@@ -0,0 +1,11 @@
+#input
+userName = 'Ravi Brennon C. Achay'
+userAdress = 'Purok Santan, Vito, Minglanilla, Cebu'
+telnum = '123 4567'
+phonenum = '1234 123 1234'
+
+#output
+print("Name:", userName)
+print("Adress:", userAdress)
+print("Telephone Number:", telnum)
+print("Phone Number:", phonenum)
